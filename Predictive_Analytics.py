91dc6eda147523a475adc2eb183d2618c996e667
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-12-12 19:57:07 -0300

    Import Libraries

--- Predictive_Analytics.py
+++ Predictive_Analytics.py
@@ -5,5 +5,6 @@ from prophet.plot import plot_plotly
 from plotly import graph_objects as go
 from datetime import date
 
-
+# to run the application open the file folder, with the right button click on git bash here and type the command: streamlit run Predictive_Analytics.py
+st.title("Aplicação de Análise Preditiva")
 
