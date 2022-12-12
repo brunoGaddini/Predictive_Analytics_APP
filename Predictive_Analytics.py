df4afe7c633ed4b68b4d3f009c85db0491d1c396
bruno_gaddini <bruno_gaddini@hotmail.com>
2022-12-11 21:50:11 -0300

    Import Libraries

--- Predictive_Analytics.py
+++ Predictive_Analytics.py
@@ -1,5 +1,9 @@
-import streamlit
-import prophet
-import pandas
+import streamlit as st
+import yfinance as yf
+from prophet import Prophet
+from prophet.plot import plot_plotly
+from plotly import graph_objects as go
+from datetime import date
+
 
 
